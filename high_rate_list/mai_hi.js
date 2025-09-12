189754701a224cd8efe815c872ffb573df450b26
FuyuGitsune <charliemouse60@gmail.com>
2025-09-12 23:56:55 +0900

    より見やすく改善

--- high_rate_list/mai_hi.js
+++ high_rate_list/mai_hi.js
@@ -10,29 +10,38 @@ function change_state(stat){
 
 function change_achi(id){
     let tag = document.getElementById(id);
+    let text = document.getElementById(id + "text");
     switch (check_stat){
         case 0:
             tag.className = "s";
+            text.className = "stext";
             break;
         case 1:
             tag.className = "sp";
+            text.className = "sptext";
             break;
         case 2:
             tag.className = "ds";
+            text.className = "dstext";
             break;
         case 3:
             tag.className = "dsp";
+            text.className = "dsptext";
             break;
         case 4:
             tag.className = "ts";
+            text.className = "tstext";
             break;
         case 5:
             tag.className = "tsp";
+            text.className = "tsptext";
             break;
         case 6:
             tag.className = "none";
+            text.className = "nonetext";
             break;
         default:
             tag.className = "none";
+            text.className = "nonetext";
     }
 }
